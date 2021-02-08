8c52c50826893901d23d2bef4ed9b30dafed7a1a
Kremernator <madzapp@web.de>
2021-02-08 11:04:17 +0100

    Declaration1

--- script_01.js
+++ script_01.js
@@ -1,4 +1,14 @@
 /***** Variablen 01 *******/
-
+/** 
 console.log("tach");
 console.log("auch");
+*/
+/*Declaration + Zuweisung 1
+let firstName = "Gabi";
+let secondName = "Popabi";
+//firstName = "Klaus";
+console.log(firstName);
+console.log(secondName);
+console.log(firstName + " " + secondName);
+*/
+
